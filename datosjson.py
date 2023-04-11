ade36576585f9bde043363fe8d5c2ae4c535e643
NetAcad DEVASC <changeme@example.com>
2023-04-11 22:56:09 +0000

    caso estudio_1

---
+++ datosjson.py
@@ -0,0 +1,8 @@
+import json
+import yaml
+
+with open ('myfile.json','r') as json_file:
+    ourjson = json.load(json_file)
+
+print("The access token is: {}".format(ourjson['access_token']))
+print("The token expires in {} seconds.".format(ourjson['expires_in']))
